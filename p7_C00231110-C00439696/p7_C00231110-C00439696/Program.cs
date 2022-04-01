0f8e5ad5ebb394f2c0a8c86d4f8b18320c143cdc
Cameron Washington <89416312+C00439696@users.noreply.github.com>
2022-04-01 15:25:09 -0500

    Cameron's half of Project 7

--- p7_C00231110-C00439696/p7_C00231110-C00439696/Program.cs
+++ p7_C00231110-C00439696/p7_C00231110-C00439696/Program.cs
@@ -3,4 +3,66 @@
 // CMPS 358
 // Project: p7
 
-Console.WriteLine("Hello, World!");
+static void isDiscontinued()
+{
+    using var db = new smallbusiness.smallbusiness();
+    {
+        var results =
+            from p in db.Products
+            where p.IsDiscontinued.ToString() == "1"
+            select p.ProductName;
+
+        Console.WriteLine("Products that are discontinued: ");
+        foreach (var p in results)
+        {
+            Console.WriteLine(p + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+static void ListCustomersInfo(string country)
+{
+    using var db = new smallbusiness.smallbusiness();
+    {
+        var results =
+            from co in db.Customers
+            where co.Country == country
+            select new {co.FirstName, co.LastName, co.Phone};
+        if (results.Count() == 0)
+        {
+            Console.WriteLine($"No customers in {country}.");
+            return;
+        }
+
+        Console.WriteLine($"Customers in {country}:");
+        foreach (var c in results)
+            Console.WriteLine($" {c}");
+        Console.WriteLine();
+    }
+}
+
+static void ListSupplierInfo(string country)
+{
+    using var db = new smallbusiness.smallbusiness();
+    {
+        var results =
+            from su in db.Suppliers
+            where su.Country == country
+            select new {su.Id, su.CompanyName, su.Phone, su.Fax, su.City };
+        if (results.Count() == 0)
+        {
+            Console.WriteLine($"No suppliers in {country}.");
+            return;
+        }
+
+        Console.WriteLine($"Suppliers in {country}:");
+        foreach (var c in results)
+            Console.WriteLine($" {c}");
+        Console.WriteLine();
+    }
+}
+
+isDiscontinued();
+ListCustomersInfo("Canada");
+ListSupplierInfo("USA");
